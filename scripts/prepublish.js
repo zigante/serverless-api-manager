0466260efcf078b42ec177af9dcc96ac68e7d74a
zigante <zigante.pedro@gmail.com>
2021-10-20 20:40:08 -0300

    Create script to handle with publishing

---
+++ scripts/prepublish.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const ORIG_PKG_PATH = path.resolve(__dirname, '../package.json');
+const CACHED_PKG_PATH = path.resolve(__dirname, '../cached-package.json');
+const pkgData = require(ORIG_PKG_PATH);
+
+fs.writeFile(CACHED_PKG_PATH, JSON.stringify(pkgData));
+
+delete pkgData.devDependencies;
+delete pkgData.scripts;
+
+fs.writeFile(ORIG_PKG_PATH, JSON.stringify(pkgData, null, 2));
