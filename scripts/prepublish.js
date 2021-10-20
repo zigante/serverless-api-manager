c34fd4d53de76edc277bc75407a869a635b43e8d
zigante <zigante.pedro@gmail.com>
2021-10-20 20:51:17 -0300

    Fix deploy script

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -5,9 +5,13 @@ const ORIG_PKG_PATH = path.resolve(__dirname, '../package.json');
 const CACHED_PKG_PATH = path.resolve(__dirname, '../cached-package.json');
 const pkgData = require(ORIG_PKG_PATH);
 
-fs.writeFile(CACHED_PKG_PATH, JSON.stringify(pkgData));
+fs.writeFile(CACHED_PKG_PATH, JSON.stringify(pkgData), err => {
+  if (err) throw err;
+});
 
 delete pkgData.devDependencies;
 delete pkgData.scripts;
 
-fs.writeFile(ORIG_PKG_PATH, JSON.stringify(pkgData, null, 2));
+fs.writeFile(ORIG_PKG_PATH, JSON.stringify(pkgData, null, 2), err => {
+  if (err) throw err;
+});
